9c9be5ae3a1015e9674b6c6d8f58fa3529ad606a
abatyuk <andrii.batyuk@gmail.com>
2022-02-20 19:18:39 +0200

    Есть некоторый текст. Посчитайте в этом тексте количество предложений и выведите на экран полученный результат.

---
+++ Homework10.1.py
@@ -0,0 +1,30 @@
+# Задание 2
+# Пользователь вводит с клавиатуры некоторый текст,
+# после чего пользователь вводит список зарезервированных
+# слов. Необходимо найти в тексте все зарезервированные
+# слова и изменить их регистр на верхний. Вывести на
+# экран измененный текст.
+
+
+
+
+
+
+# Variables
+
+user_str = str(input("Input some string: "))
+user_word = list(input("Input some worlds: "))
+
+print(user_word)
+# c = ''
+# # Cycle for removing spaces
+# for i in a:
+#     if i == ' ':
+#         continue  # Removing spaces
+#     b = b + i
+# c = b[::-1]  # Create reverse string
+# # Compare with lowercase and print results
+# if c.lower() == b.lower():
+#     print(a, "is palindrome!")
+# else:
+#     print(a, "is not palindrome((")
