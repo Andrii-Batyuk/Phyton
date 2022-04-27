6363046e1b5502d3c8e5323c736d67de314a104a
abatyuk <andrii.batyuk@gmail.com>
2022-04-27 11:44:09 +0200

    Home Work Jira __Task_1__

--- JiraHW1.py
+++ JiraHW1.py
@@ -8,6 +8,10 @@ driver.get('http://www.yanigen.com.ua')
 # 2 Maximize window
 driver.maximize_window()
 
-# 3 Close site
+# 3 Make screenshot
+time.sleep(5)
+driver.save_screenshot('yanigen_home_page.png')
+
+# 4 Close site
 time.sleep(10)
 driver.close()
