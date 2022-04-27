47d4a4d3b9d2d94f5fb91d5fb7fac720cabc7ccc
abatyuk <andrii.batyuk@gmail.com>
2022-04-27 11:35:16 +0200

    Home Work Jira __Task_1__

---
+++ JiraHW1.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+import time
+
+# 1 Open site yanigen.com.ua
+driver = webdriver.Chrome()
+driver.get('http://yanigen.com.ua')
+
+# 2 Maximize window
+driver.maximize_window()
+
+# 3 Close site
+time.sleep(10)
+driver.close()
