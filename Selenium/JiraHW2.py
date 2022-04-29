e5b0bc4e0ac5fa31d4336c17d8fafb2f25615133
abatyuk <andrii.batyuk@gmail.com>
2022-04-29 14:26:45 +0200

    Home Work Jira __Task_2__

--- Selenium/JiraHW2.py
+++ Selenium/JiraHW2.py
@@ -105,8 +105,3 @@ contacts_en_button.click()
 
 driver.quit()
 
-
-
-ru_lang = '//li[@class]/a[contains(text(),"RU")]' #ru_button
-eng_lang = '//li[@class]/a[contains(text(),"EN")]' #eng_button
-ua_lang = '//li[@class]/a[contains(text(),"UA")]' #ua_button
